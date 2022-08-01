02832eb87e92adeaa5b9c2efded01ae353698cc1
shivajighasti/04 <shivajighasti97@gmail.com>
2022-08-01 21:29:53 +0530

    Test cases using constructor

---
+++ MoodAnalyserTest.java
@@ -0,0 +1,31 @@
+package com.bridgelabz.moodanalysertest;
+
+import com.bridgelabz.moodanalyser.MoodAnalyser;
+import org.junit.Test;
+import org.junit.Assert;
+public class MoodAnalyserTest {
+    // -----------------Test for Sad-----------
+    @Test
+    public void givenMessageWhenSadShouldBeReturnSad() {
+        // create object
+        MoodAnalyser moodeAnalyser = new MoodAnalyser();
+        // pass Sad message
+        String message = "I am in Sad Mood";
+        String expectedValue = "SAD";
+        String mood = moodeAnalyser.analyseMood(message);
+        Assert.assertEquals(expectedValue, mood);
+    }
+
+    // ---------------Test for Happy---------------
+    @Test
+    public void givenMessageWhenHappyShouldBeReturnHappy() {
+        // create object
+        MoodAnalyser moodeAnalyser = new MoodAnalyser();
+        // pass Sad message
+        String message = "I am in Happy Mood";
+        String expectedValue = "HAPPY";
+        String mood = moodeAnalyser.analyseMood(message);
+        Assert.assertEquals(expectedValue, mood);
+    }
+}
+
