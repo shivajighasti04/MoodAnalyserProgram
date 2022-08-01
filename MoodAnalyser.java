e4848f1856dd17b78b82e0cc2a6d427a5832a99e
shivajighasti/04 <shivajighasti97@gmail.com>
2022-08-01 19:12:45 +0530

    Refactored the code to take the mood message in Constructor

---
+++ MoodAnalyser.java
@@ -0,0 +1,31 @@
+package com.bridgelabz.moodanalyser;
+
+public class MoodAnalyser {
+    // Variable Declaration
+    static String message;
+
+    // Default Constructor
+    public MoodAnalyser() {
+        message = null;
+    }
+
+    // Parameterised Constructor
+    public MoodAnalyser(String message) {
+        this.message = message;
+    }
+
+    // Setter
+    public static void setMessage(String message) {
+        MoodAnalyser.message = message;
+    }
+
+    // Check mood wheather Happy or Sad
+    public String analyseMood(String message) {
+        if (message.equalsIgnoreCase("I am in sad mood"))
+            return "SAD";
+        else if (message.equalsIgnoreCase("I am in happy mood"))
+            return "HAPPY";
+        else
+            return "No Match";
+    }
+}
