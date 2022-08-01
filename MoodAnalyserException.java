4bf58ad3bb877e0bd51080271ea14499a6840512
shivajighasti/04 <shivajighasti97@gmail.com>
2022-08-01 22:34:53 +0530

    Program to check custom exception

--- MoodAnalyserException.java
+++ MoodAnalyserException.java
@@ -1,2 +1,14 @@
-package com.bridgelabz.moodanalyser;public class MoodAnalyserException {
+package com.bridgelabz.moodanalyser;
+
+public class MoodAnalyserException extends Exception {
+    //variable declare and assign
+    enum ExceptionType {ENTERED_NULL, ENTERED_EMPTY}
+
+    ExceptionType type;
+
+    //field constructor
+    public MoodAnalyserException(String mood, ExceptionType type) {
+        super(mood);
+        this.type = type;
+    }
 }
