0b46ce32dc1f371d3ce091bdf9953ad7638691dd
shivajighasti/04 <shivajighasti97@gmail.com>
2022-08-01 22:11:58 +0530

    Program to Handle Exception if User Provides Invalid Mood

---
+++ MoodAnalyser.java
@@ -0,0 +1,35 @@
+package com.bridgelabz.moodanalyser;
+
+public class MoodAnalyser {
+    // Variable Declaration
+    static String message;
+
+    // Default Constructor
+    public MoodAnalyser() {
+        message = null;
+    }
+
+    // Parameterised Constructor
+    public MoodAnalyser(String message) {
+        this.message = message;
+    }
+
+    // Setter
+    public static void setMessage(String message) {
+        MoodAnalyser.message = message;
+    }
+
+    // Check mood wheather Happy or Sad
+    public String analyseMood(String message) {
+
+        try {
+            if (message.equalsIgnoreCase("I am in sad mood"))
+                return "SAD";
+            else
+                return "HAPPY";
+
+        } catch (NullPointerException exception) {
+            return "Happy";
+        }
+    }
+}
