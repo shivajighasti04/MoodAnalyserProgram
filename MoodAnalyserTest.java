3f474950d44691c6c820840a8219360cb80aaec3
shivajighasti/04 <shivajighasti97@gmail.com>
2022-08-01 14:43:02 +0530

    Test Case for analyseMood method

---
+++ MoodAnalyserTest.java
@@ -0,0 +1,26 @@
+package com.bridgelabz.moodanalysertest;
+
+import com.bridgelabz.moodanalyser.MoodAnalyser;
+import org.junit.Test;
+import org.junit.Assert;
+public class MoodAnalyserTest {
+    // -----------------Test for Sad-----------
+    @Test
+    public void givenMessageWhenSadShouldBeReturnSad() {
+        // create object
+        MoodAnalyser moodeAnalyser = new MoodAnalyser();
+        String expectedValue = "SAD";
+        String mood = moodeAnalyser.analyseMood("I am in Sad Mood");
+        Assert.assertEquals(expectedValue, mood);
+    }
+
+    // ---------------Test for Happy---------------
+    @Test
+    public void givenMessageWhenHappyShouldBeReturnHappy() {
+        // create object
+        MoodAnalyser moodeAnalyser = new MoodAnalyser();
+        String expectedValue = "HAPPY";
+        String mood = moodeAnalyser.analyseMood("I am in Happy Mood");
+        Assert.assertEquals(expectedValue, mood);
+    }
+}
